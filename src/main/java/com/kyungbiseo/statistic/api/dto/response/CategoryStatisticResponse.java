17c78e671edddfd391b647d56922632475888b4d
toneofrain <toneofrain@gmail.com>
2024-03-27 19:35:15 +0900

    Chore: add greetings package (#3)

--- src/main/java/com/kyungbiseo/statistic/api/dto/response/CategoryStatisticResponse.java
+++ src/main/java/com/kyungbiseo/statistic/api/dto/response/CategoryStatisticResponse.java
@@ -6,17 +6,19 @@ public record CategoryStatisticResponse(Map<String, String> categories,
 										Map<String, Integer> amounts) {
 	public CategoryStatisticResponse() {
 		this(Map.ofEntries(
-			Map.entry("ageGroup", "TWENTIES_LATE"),
-			Map.entry("gender", "MALE"),
-			Map.entry("occupation", "STUDENT"),
-			Map.entry("relationship", "CLOSE_FRIEND"),
-			Map.entry("eventType", "BIRTHDAY")
-		),
-		Map.ofEntries(
-			Map.entry("max", 300000),
-			Map.entry("min", 50000),
-			Map.entry("avg",70000),
-			Map.entry("med",120000)
-		));
+				Map.entry("ageGroup", "TWENTIES_LATE"),
+				Map.entry("gender", "MALE"),
+				Map.entry("occupation", "STUDENT"),
+				Map.entry("relationship", "CLOSE_FRIEND"),
+				Map.entry("eventType", "BIRTHDAY"),
+				Map.entry("eventPriority", "HIGH")
+			),
+			Map.ofEntries(
+				Map.entry("max", 300000),
+				Map.entry("min", 50000),
+				Map.entry("avg",70000),
+				Map.entry("med",120000)
+			)
+		);
 	}
 }
