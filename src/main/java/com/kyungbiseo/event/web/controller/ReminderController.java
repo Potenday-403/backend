7ba111dbba249942daeac13e5c5ee09e5def2e63
toneofrain <toneofrain@gmail.com>
2024-03-28 19:39:26 +0900

    Feat: mock up reminder api (#8)

---
+++ src/main/java/com/kyungbiseo/event/web/controller/ReminderController.java
@@ -0,0 +1,18 @@
+package com.kyungbiseo.event.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kyungbiseo.event.web.dto.response.ReminderResponse;
+
+@RestController
+@RequestMapping("/reminder")
+public class ReminderController {
+
+	@GetMapping
+	public ResponseEntity<ReminderResponse> reminder() {
+		return ResponseEntity.ok(new ReminderResponse());
+	}
+}
---
+++ src/main/java/com/kyungbiseo/event/web/dto/response/ReminderResponse.java
@@ -0,0 +1,19 @@
+package com.kyungbiseo.event.web.dto.response;
+
+import java.util.List;
+
+import com.kyungbiseo.event.domain.EventPriority;
+
+public record ReminderResponse(List<ReminderEvent> events,
+							   int count) {
+	public ReminderResponse() {
+		this(List.of(
+			new ReminderEvent(17L, "강인이 생일", "2024-03-27T00:00", EventPriority.CRUCIAL),
+			new ReminderEvent(18L, "조카 돌잔치", "2024-03-28T00:00", EventPriority.IMPORTANT),
+			new ReminderEvent(19L, "팀장 결혼식", "2024-03-29T00:00", EventPriority.NORMAL)
+		),
+			3);
+	}
+
+
+}
