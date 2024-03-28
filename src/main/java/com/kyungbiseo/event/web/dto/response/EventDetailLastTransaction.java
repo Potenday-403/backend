856f103abcf1f372b2bf5865d25fc7e8b53eea1c
toneofrain <toneofrain@gmail.com>
2024-03-29 08:36:07 +0900

    Refactor: rename tribute entity

--- src/main/java/com/kyungbiseo/event/web/dto/response/EventDetailLastTransaction.java
+++
@@ -1,9 +0,0 @@
-package com.kyungbiseo.event.web.dto.response;
-
-import com.kyungbiseo.event.domain.EventType;
-
-public record EventDetailLastTransaction(String date,
-										 EventType eventType,
-										 String giftName,
-										 int price) {
-}
